ec8a58b32ec80044f6b65d6d0e33499549bc3b64
sijanthapa171 <thapasijan329@gmail.com>
2026-06-06 18:18:15 +0545

    [FEAT]: update old flake with new for hammro school application

--- flake.nix
+++ flake.nix
@@ -1,98 +1,133 @@
 {
   description = "Hammro School Application - Development Environment";
 
-  inputs.nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/0.1"; # unstable Nixpkgs
+  inputs.nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/0.1";
 
-  outputs =
-    { self, ... }@inputs:
+  outputs = { self, ... }@inputs:
 
-    let
-      javaVersion = 21; # Match the Java release used by the Maven build
+  let
+    javaVersion = 21;
 
-      supportedSystems = [
-        "x86_64-linux"
-        "aarch64-linux"
-        "x86_64-darwin"
-        "aarch64-darwin"
-      ];
-      forEachSupportedSystem =
-        f:
-        inputs.nixpkgs.lib.genAttrs supportedSystems (
-          system:
-          f {
-            pkgs = import inputs.nixpkgs {
-              inherit system;
-              overlays = [ inputs.self.overlays.default ];
-            };
-          }
-        );
-    in
-    {
-      overlays.default =
-        final: prev:
-        let
-          jdk = prev."jdk${toString javaVersion}";
-        in
-        {
-          inherit jdk;
-          maven = prev.maven.override { jdk_headless = jdk; };
+    supportedSystems = [
+      "x86_64-linux"
+      "aarch64-linux"
+      "x86_64-darwin"
+      "aarch64-darwin"
+    ];
+
+    forEachSupportedSystem = f:
+      inputs.nixpkgs.lib.genAttrs supportedSystems (system:
+        f {
+          pkgs = import inputs.nixpkgs {
+            inherit system;
+            overlays = [ self.overlays.default ];
+          };
+        }
+      );
+
+  in
+  {
+    overlays.default = final: prev:
+      let
+        jdk = prev."jdk${toString javaVersion}";
+      in
+      {
+        inherit jdk;
+        maven = prev.maven.override { jdk_headless = jdk; };
+      };
+
+    devShells = forEachSupportedSystem ({ pkgs }: {
+      default = pkgs.mkShell {
+        name = "school-management-system-dev";
+
+        packages = with pkgs; [
+
+          # =====================
+          # Build tools
+          # =====================
+          jdk
+          maven
+          git
+          curl
+          wget
+          gcc
+          gnumake
+          jdt-language-server
+
+          # =====================
+          # JavaFX runtime deps
+          # =====================
+
+          # X11 / OpenGL (fixes libXxf86vm + rendering issues)
+          xorg.libXxf86vm
+          xorg.libXtst
+          xorg.libXi
+          xorg.libXrandr
+          xorg.libXrender
+          xorg.libXcursor
+          xorg.libXext
+
+          # GTK backend (fixes glassgtk3 + gthread)
+          glib
+          gtk3
+          gdk-pixbuf
+          pango
+          cairo
+          atk
+
+          # graphics stack
+          mesa
+          fontconfig
+          freetype
+        ];
+
+        env = {
+          JAVA_HOME = "${pkgs.jdk}";
+          MAVEN_OPTS = "-Xmx2048m";
+          PROJECT_DIR = builtins.toString ./.;
+          DATABASE_FILE = "data/hammroschool";
+
+          # IMPORTANT: native libs for JavaFX
+          LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath [
+            pkgs.xorg.libXxf86vm
+            pkgs.xorg.libXtst
+            pkgs.xorg.libXi
+            pkgs.xorg.libXrandr
+            pkgs.xorg.libXrender
+            pkgs.xorg.libXcursor
+            pkgs.xorg.libXext
+            pkgs.glib
+            pkgs.gtk3
+            pkgs.mesa
+          ];
         };
+      };
+    });
 
-      devShells = forEachSupportedSystem (
-        { pkgs }:
-        {
-          default = pkgs.mkShell {
-            name = "school-management-system-dev";
+    apps = forEachSupportedSystem ({ pkgs }: {
+      build = {
+        type = "app";
+        program = toString (pkgs.writeShellScript "build" ''
+          cd ${builtins.toString ./.}
+          ${pkgs.maven}/bin/mvn clean install
+        '');
+      };
 
-            packages = with pkgs; [
-              jdk
-              maven
-              git
-              curl
-              wget
-              gcc
-              gnumake
-              jdt-language-server
-            ];
+      run = {
+        type = "app";
+        program = toString (pkgs.writeShellScript "run" ''
+          cd ${builtins.toString ./.}
+          ${pkgs.maven}/bin/mvn clean javafx:run
+        '');
+      };
 
-            env = {
-              JAVA_HOME = "${pkgs.jdk}";
-              MAVEN_OPTS = "-Xmx2048m";
-              PROJECT_DIR = builtins.toString ./.;
-              DATABASE_FILE = "data/hammroschool";
-            };
-          };
-        }
-      );
-
-      # Flake apps for convenient project execution
-      apps = forEachSupportedSystem (
-        { pkgs }:
-        {
-          build = {
-            type = "app";
-            program = toString (pkgs.writeShellScript "build" ''
-              cd ${builtins.toString ./.}
-              ${pkgs.maven}/bin/mvn clean install
-            '');
-          };
-
-          run = {
-            type = "app";
-            program = toString (pkgs.writeShellScript "run" ''
-              cd ${builtins.toString ./.}
-              ${pkgs.maven}/bin/mvn clean javafx:run
-            '');
-          };
-
-          package = {
-            type = "app";
-            program = toString (pkgs.writeShellScript "package" ''
-              cd ${builtins.toString ./.}
-              ${pkgs.maven}/bin/mvn clean package
-            '');
-          };
-        }
-      );
-    };
+      package = {
+        type = "app";
+        program = toString (pkgs.writeShellScript "package" ''
+          cd ${builtins.toString ./.}
+          ${pkgs.maven}/bin/mvn clean package
+        '');
+      };
+    });
+  };
 }
